222bed6efe661cbf8c3598d4a9746cb2040fbc49
Peter Phanvan <pfunnyyy@gmail.com>
2022-03-28 11:21:11 +0300

    formatting doc

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
 const ObjectID = require("mongodb").ObjectID;
 
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
+
 const app = express();
 app.set("view engine", "pug");
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -54,5 +60,5 @@ myDB(async (client) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
